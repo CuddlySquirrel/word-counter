8ed73de51e910fdab10802a3eb7a912124b0308c
CuddlySquirrel <chrisfluur@hushmail.com>
2015-04-07 01:52:32 -0500

    updated url validator

--- app/validators/url_validator.rb
+++ app/validators/url_validator.rb
@@ -1,8 +1,8 @@
 class UrlValidator < ActiveModel::EachValidator
   def validate_each(record, attribute, value)
     if !value.blank?
-      record.errors[attribute] << "must begin with http://" unless 
-      value.match(/^http:\/\//)
+      record.errors[attribute] << "must begin with http:// or https://" unless 
+      value.match(/^https?:\/\//)
     end
   end
 end
