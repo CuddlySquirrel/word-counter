0889da6ce7e9b992a42febb87a56dee8c1fb59a8
CuddlySquirrel <chrisfluur@hushmail.com>
2015-08-29 15:06:40 -0500

    moved display logic to the urls helper

--- app/helpers/url_helper.rb
+++ app/helpers/url_helper.rb
@@ -1,6 +1,13 @@
 module UrlHelper
+
   def helper_most_popular_word(url)
     most_popular = url.popularities.order(:occurrences).last
     { value: most_popular.word.value, occurrences: most_popular.occurrences }
   end
+  
+  def helper_top_ten_words(url)
+    popularities = url.popularities.order(occurrences: :desc)
+    popularities.map{|x|{value: x.word.value, occurrences: x.occurrences}}
+  end
+
 end
