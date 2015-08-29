42720046d3be6221acadee12928cb49c866e613c
CuddlySquirrel <chrisfluur@hushmail.com>
2015-08-29 13:51:48 -0500

    refactored url model

--- app/models/url.rb
+++ app/models/url.rb
@@ -4,7 +4,7 @@ class Url < ActiveRecord::Base
 
   validates :value, presence: true, uniqueness: true, url: true
 
-  after_save :after_save
+  after_save :associate_words_and_popularities
 
   def most_popular
     top_ten_words.shift
@@ -17,9 +17,12 @@ class Url < ActiveRecord::Base
 
   private
 
-  def after_save
-    counted_words = Scrape::ExtractTextAndCountWords.new(self[:value]).execute()
-    scrape_command.each do |word,occurrences|
+  def counted_words
+    Scrape::ExtractTextAndCountWords.new(self[:value]).run()
+  end
+
+  def associate_words_and_popularities
+    counted_words().each do |word,occurrences|
       word = Word.find_or_create_by( value: word )
       Popularity.create(url_id: self[:id], word_id: word.id, occurrences: occurrences )
     end
--- spec/models/url_spec.rb
+++ spec/models/url_spec.rb
@@ -3,9 +3,22 @@ require 'rails_helper'
 describe Url do
   
   describe 'public_methods' do
+
+    it { expect(subject).to respond_to(:popularities) }
+    it { expect(subject).to respond_to(:words) }
     it { expect(subject).to respond_to(:value) }
     it { expect(subject).to respond_to(:most_popular) }
     it { expect(subject).to respond_to(:top_ten_words) }
+
+  end
+
+  describe 'validations' do
+
+    it 'requires value' do 
+      url = Url.new(value: '')
+      expect(url).to_not be_valid
+    end
+
   end
 
 
