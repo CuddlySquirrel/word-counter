67810b3bc542a23076cb9edb9677801344294d4d
CuddlySquirrel <chrisfluur@hushmail.com>
2015-08-28 19:51:03 -0500

    added spec/factories/factories_spec.rb

--- spec/factories/factories_spec.rb
+++ spec/factories/factories_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 FactoryGirl.factories.map(&:name).each do |factory_name|
   describe "factory #{factory_name}" do
      it 'is valid' do
-      expect(FactoryGirl.build(factory_name)).to be_valid
+      build(factory_name).should be_valid
      end
   end
 end
