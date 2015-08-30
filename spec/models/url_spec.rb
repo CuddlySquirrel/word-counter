39a170b703d1acd8089aab229a6b321b19697980
CuddlySquirrel <chrisfluur@hushmail.com>
2015-08-30 08:06:15 -0500

    removed old tests from url spec

--- spec/models/url_spec.rb
+++ spec/models/url_spec.rb
@@ -7,8 +7,6 @@ describe Url do
     it { expect(subject).to respond_to(:popularities) }
     it { expect(subject).to respond_to(:words) }
     it { expect(subject).to respond_to(:value) }
-    it { expect(subject).to respond_to(:most_popular) }
-    it { expect(subject).to respond_to(:top_ten_words) }
 
   end
 
