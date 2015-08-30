e5de564f1731ecbbe383e6df7b9223c99f249b69
CuddlySquirrel <chrisfluur@hushmail.com>
2015-08-30 16:43:23 -0500

    finished html from url spec

--- spec/services/html_from_url_spec.rb
+++ spec/services/html_from_url_spec.rb
@@ -1,25 +1,27 @@
 require 'rails_helper'
 
 describe HTMLFromUrl do
+
+  let(:text){ 'body' }
  
-  let(:html){ '<html><body>body</body></html>' }
+  let(:html){ "<html><body>#{text}</body></html>" }
 
-  let(:html_from_url_service){ HTMLFromUrl.new('url string') }
+  let(:service){ HTMLFromUrl.new('url string') }
 
-  let(:nokogiri_html_document){ html_from_url_service.create() }
+  let(:nokogiri_html_document){ service.create() }
 
   describe 'public methods' do
-    it { expect(html_from_url_service).to respond_to(:create) }
+    it { expect(service).to respond_to(:create) }
   end
 
   it 'returns a nokogiri html object' do
-    html_from_url_service.stub(:html_get){ html }
+    service.stub(:html_get){ html }
     expect(nokogiri_html_document).to be_a(Nokogiri::HTML::Document)
   end
 
   it 'has the right content' do
-    html_from_url_service.stub(:html_get){ html }
-    expect(nokogiri_html_document.text).to eq('body')
+    service.stub(:html_get){ html }
+    expect(nokogiri_html_document.text).to eq(text)
   end
 
 end
